de96bf00bfde10bbb596f51b80122ba2918eb0cc
wadkan <zsoltgaramszegi@gmail.com>
2021-01-06 10:22:51 +0100

    working first list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// import {v4 as uuid} from "uuid"; 
+import axios from 'axios';
+
+import Navbar from './components/layout/Navbar'
+import PokemonsList from './components/PokemonsList';
+
+class App extends Component{
+  state = {
+    todos: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res => this.setState({ todos: res.data }))
+  }
+
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    } )})
+  }
+
+  // Delete Todo
+  delTodo = (id) => {
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }));
+  }
+
+  // AddTodo
+  addTodo = (title) => {
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title,
+      completed: false
+    })
+      .then(res => this.setState({ todos:
+        [...this.state.todos, res.data] }));
+    }
+
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Navbar />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <PokemonsList todos={this.state.todos} markComplete={this.markComplete}
+                delTodo={this.delTodo}/>
+              </React.Fragment>
+            )} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
+
---
+++ src/components/PokemonsList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PokemonsList extends Component {
+    render() {
+        return (
+            <div>
+                <p>LIST::::</p>
+            </div>
+        )
+    }
+}
+
+export default PokemonsList
