4b66367801bd363ebf0058aaa7b72a0684b6e56d
wadkan <zsoltgaramszegi@gmail.com>
2021-01-07 10:48:57 +0100

    App.js working with hook!!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 
 import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import axios from 'axios';
 
@@ -9,38 +10,35 @@ import PokeList from './components/PokeList';
 import PokeDetail from './components/PokeDetail';
 import PokeTypes from './components/TypeList';
 
-class App extends Component{
-  state = {
-    pokeList: [],
-    typeList: []
-  }
-
-  componentDidMount() {
+const App = props => {
+  const [pokeList, setPokeList] = useState([]);
+  const [typeList, setTypeList] = useState([])
+  
+  useEffect(() => {
     axios.get('https://pokeapi.co/api/v2/pokemon')
-      .then(res => this.setState({ pokeList: res.data.results }));
+      .then(res => setPokeList(res.data.results));
     axios.get('https://pokeapi.co/api/v2/type')
-      .then(res => this.setState({ typeList: res.data.results }));
-  }
+    .then(res => setTypeList(res.data.results));
+  }, []);
 
-  render(){
-    return (
-      <Router>
-        <div className="App">
-          <div className="container">
-            <Navbar />
-            <Route path="/poke-list" render={props => (
-              <PokeList pokeList={this.state.pokeList}/>)} />
-            <Route path="/poke-types" render={props => (
-              <PokeTypes typeList={this.state.typeList}/>)} />
-            <Route path='/pokemon/:id' render={props => (
-              <PokeDetail />
-            )}></Route>
-          </div>
+  let content = (
+    <Router>
+      <div className="App">
+        <div className="container">
+          <Navbar />
+          <Route path="/poke-list" render={props => (
+            <PokeList pokeList={pokeList}/>)} />
+          <Route path="/poke-types" render={props => (
+            <PokeTypes typeList={typeList}/>)} />
+          <Route path='/pokemon/:id' render={props => (
+            <PokeDetail />
+          )}></Route>
         </div>
-      </Router>
-    );
+      </div>
+    </Router>
+  );
+  return content;
   }
-}
 
 export default App;
 
